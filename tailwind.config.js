cbbc6b5fdb0120b7b999588607d67854825a7004
Rushail101 <149574749+Rushail101@users.noreply.github.com>
2026-08-27 20:22:17 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
@@ -8,10 +7,9 @@ export default {
         brand: {
           50: '#fff7ed',
           100: '#ffedd5',
-          400: '#fb923c',
-          500: '#f97316',
-          600: '#ea580c',
-          700: '#c2410c',
+          500: '#ea580c',
+          600: '#c2410c',
+          700: '#9a3412',
         }
       }
     },
